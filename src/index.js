b62a674b50ab4dd0d19ce8ac3ed4c0b21b30feb3
guguducken <1173781832@qq.com>
2022-08-09 11:41:51 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const github_token = core.getInput("SIZE_TOKEN");
+// const github_token = core.getInput("SIZE_TOKEN");
 
 async function run() {
 
     try {
         //get a octokit client
-        const octokit = github.getOctokit(github_token);
+        // const octokit = github.getOctokit(github_token);
 
         //get github context
         const context = github.context;
