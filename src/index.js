64f22fda4693ceb2d34519bbdf74eb144cec62e2
guguducken <1173781832@qq.com>
2022-08-09 11:32:16 +0800

    init and test

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const github_token = core.getInput("SIZE_TOKEN");
+
+async function run() {
+
+    try {
+        //get a octokit client
+        const octokit = github.getOctokit(github_token);
+
+        //get github context
+        const context = github.context;
+
+        //get pull request num
+        const num = context?.payload?.pull_request?.number;
+
+        const labels = context?.pull_request?.labels;
+
+        core.info(labels);
+    } catch (err) {
+        core.setFailed(err.message);
+    }
+}
+
+run();
