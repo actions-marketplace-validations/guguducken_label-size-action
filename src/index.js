0afd36d60e80ca936ec3c5c55cfdb6734831d6d1
guguducken <1173781832@qq.com>
2022-08-09 13:05:12 +0800

    test

--- src/index.js
+++ src/index.js
@@ -24,9 +24,10 @@ async function run() {
                 pull_number: num,
             }
         );
-        // const { labels } = pr;
+        const { labels } = pr;
         core.info(JSON.stringify(pr));
-        // core.info(JSON.stringify(labels));
+        core.info("--------------------------------------------------------");
+        core.info(JSON.stringify(labels));
     } catch (err) {
         core.setFailed(err.message);
     }
