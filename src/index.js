e84ab5ee119c1e8da811013384094ec186b7087d
guguducken <1173781832@qq.com>
2022-08-11 18:46:10 +0800

    test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const github_token = core.getInput("size_token", { required: true });
+const ignoreStr = core.getInput('ignore');
+const size = core.getInput('size');
 
 async function run() {
 
@@ -15,6 +17,12 @@ async function run() {
         //get pull request num
         const num = context.payload?.pull_request?.number;
 
+        //get repo and owner
+        const repo_name = context.repo.repo;
+        const repo_owner = context.repo.owner;
+
+        let ignore_re = getIgnoreRe(ignoreStr);
+
         const { data: files } = await octokit.rest.pulls.listFiles(
             {
                 ...context.repo,
@@ -22,9 +30,72 @@ async function run() {
             }
         );
         core.info(JSON.stringify(files));
+
+        const changeSize = getChangeSize(files);
+
+        core.info("-----------------------------------------------------");
+        core.info(changeSize);
     } catch (err) {
         core.setFailed(err.message);
     }
+}
+
+function reParse(str) {
+    let ans = "";
+    for (let index = 0; index < str.length; index++) {
+        const e = str[index];
+        if (e == "/" || e == "{" || e == "}" || e == "[" || e == "]" ||
+            e == "(" || e == ")" || e == "^" || e == "$" || e == "+" ||
+            e == "\\" || e == "." || e == "*" || e == "|" || e == "?") {
+            ans += "\\";
+        }
+        ans += e;
+    }
+    return ans
+}
+
+function getIgnoreRe(ignoreStr) {
+    if (ignoreStr === "") {
+        return undefined;
+    }
+    let set_re = new Set();
+    let t = "";
+    for (let i = 0; i < ignoreStr.length; i++) {
+        const e = ignoreStr[i];
+        if (e == ",") {
+            if (t == "") {
+                continue;
+            }
+            set_re.add(reParse(t));
+            t = "";
+        } else {
+            t += e;
+        }
+    }
+    if (t != "") {
+        set_re.add(reParse(t));
+    }
+    if (set_re.size == 0) {
+        return undefined
+    }
+    let ans_re = new Array();
+    for (const re of set_re) {
+        ans_re.push(new RegExp(re, "igm"));
+    }
+    return ans_re
+}
+
+function getChangeSize(files, ignore) {
+    let sum = 0;
+    for (const file of files) {
+        for (const re of ignore) {
+            re.lastIndex = 0;
+            if (re.test(file.filename)) {
+                sum += file.changes;
+            }
+        }
+    }
+    return sum;
 }
 
 run();
